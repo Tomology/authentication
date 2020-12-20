3ee179e34caadd3d05cdd4afb509c36c43180f3e
Thomas Sobor <sobor.thomas@yahoo.com>
2020-12-20 11:58:52 -0800

    set up session middleware

--- session-auth/app.js
+++ session-auth/app.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const session = require("express-session");
 
-const { PORT = 3000 } = process.env;
+const TWO_HOURS = 1000 * 60 * 60 * 2;
+
+const {
+  PORT = 3000,
+  NODE_ENV = "development",
+  SESS_NAME = "sid",
+  SESS_SECRET = "ssh!quite,it'asecret!",
+  SESS_LIFETIME = TWO_HOURS,
+} = process.env;
+
+const IN_PROD = NODE_ENV === "production";
 
 const app = express();
 
-app.use(session({
+app.use(
+  session({
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
     cookie: {
-        
-    }
-}))
+      maxAge: SESS_LIFETIME,
+      sameSite: "strict",
+      secure: IN_PROD,
+    },
+  })
+);
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+app.listen(PORT, (req, res) => console.log(`http://localhost:${PORT}`));
